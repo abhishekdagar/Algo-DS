8c233b4c4fb7926c7933580409b20cd1fc12a7a2
abhishekdagar <31288082+abhishekdagar@users.noreply.github.com>
2019-10-01 17:22:58 +0530

    without fuction
    without fuction and more readable and understandale

--- C/Misc/fabonacci.c
+++ C/Misc/fabonacci.c
@@ -1,31 +1,19 @@
-//Header files
-#include <stdio.h>//header input output files
-#include <stdlib.h>//header library
-//function defination
-int fabonacci(int );
-
-//main function
-
-int main() {
-    int N;
-    printf("Enter the no of terms: ");
-    scanf("%d",&N);
-    fabonacci(N); //function call
-    return 0;
-}
-
-//function body
-
-int fabonacci(int M) {
-    int a=0,b=1,i,c;
-    printf("Fabonacci series is:\n%d\n%d",a,b);
-    //for loop to find all fabonnaci number
-    for (i=0;i<M-2;i++) {
-        c=a+b;//fabonacci series is addition of previous two numbers
-        a=b;
-        b=c;
-        printf("\n%d",c);
+#include<stdio.h>
+void main(){
+int first,second,sum,n,counter=0;
+    printf("enter number of terms\n");
+    scanf("%d",&n);
+    printf("enter first number\n");
+    scanf("%d",&first);
+    printf("enter second number\n");
+    scanf("%d",&second);
+    printf("fibonacci series: %d %d\t",first,second);
+    while(counter<n){
+        sum=first+second;
+        printf("%d",sum);
+        first=second;
+        second=sum;
+        counter++;
     }
-    return 0;
 }
 
